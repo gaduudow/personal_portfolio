bae83937b8c11812674b181ec137c9fd41eba530
Khalid Mohamed Hussein <109601738+gaduudow@users.noreply.github.com>
2023-10-06 12:05:20 +0300

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://www.@khalid69293954"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
